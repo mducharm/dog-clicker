2679a756cc1bba5aed9c8868d74a8061f5bad29b
Michael <sheveksmath@gmail.com>
2019-03-27 22:26:30 -0400

    initialized project, working on API calls with axios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <DogCard/>
+      {{breedsData}}
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DogCard from "./components/DogCard.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    DogCard
+  },
+  data() {
+    return {
+      breedsData: null
+    };
+  },
+  mounted() {
+    axios
+      .get("https://dog.ceo/api/breeds/list/all")
+      .then(response => {
+        this.breedsData = response.data.message;
+        return response.data.message;
+      })
+      .then(response => {});
   }
-}
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/DogCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <p class="card-header-text">DogCard</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DogCard",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
