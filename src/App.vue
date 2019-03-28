f295428369f0b0008a09f8dafe8c4cdfc26b5278
Michael <sheveksmath@gmail.com>
2019-03-28 19:43:42 -0400

    now queries for img src for each dog breed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="container">
-      <DogCard v-for="breedArray in breedsData" :key="breedArray">
+      <DogCard v-for="(breedArray, index) in breedsData" :key="index">
         <h1 v-if="breedArray.length > 1">{{breedArray[0] + ' ' + breedArray[1]}}</h1>
         <h1 v-else>{{breedArray[0]}}</h1>
       </DogCard>
@@ -25,19 +25,46 @@ export default {
     };
   },
   mounted() {
-    axios.get("https://dog.ceo/api/breeds/list/all").then(response => {
-      let breeds = response.data.message;
-      let breedArray = [];
-      Object.keys(breeds).forEach(breed => {
-        if (breeds[breed].length > 0) {
-          breeds[breed].forEach(subBreed => breedArray.push([subBreed, breed]));
-        } else {
-          breedArray.push([breed]);
-        }
+    axios
+      .get("https://dog.ceo/api/breeds/list/all")
+      .then(response => {
+        let breeds = response.data.message;
+        let breedArray = [];
+        Object.keys(breeds).forEach(breed => {
+          if (breeds[breed].length > 0) {
+            breeds[breed].forEach(subBreed =>
+              breedArray.push([subBreed, breed])
+            );
+          } else {
+            breedArray.push([breed]);
+          }
+        });
+        this.breedsData = breedArray;
+        return breedArray;
+      })
+      .then(breedsArray => {
+        breedsArray.forEach(breed => {
+          if (breed.length > 1) {
+            axios
+              .get(
+                `https://dog.ceo/api/breed/${breed[1]}/${
+                  breed[0]
+                }/images/random`
+              )
+              .then(response => {
+                console.log(response.data.message);
+                return response.data.message;
+              });
+          } else {
+            axios
+              .get(`https://dog.ceo/api/breed/${breed[0]}/images/random`)
+              .then(response => {
+                console.log(response.data.message);
+                return response.data.message;
+              });
+          }
+        });
       });
-      this.breedsData = breedArray;
-      return breedArray;
-    });
   }
 };
 </script>
